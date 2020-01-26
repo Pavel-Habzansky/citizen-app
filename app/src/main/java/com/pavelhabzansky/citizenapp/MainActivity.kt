df49189dddcd5c222a2284f440ef896c83dbaeda
Pavel <pavel.habzansky@eman.cz>
2020-01-26 01:19:16 +0100

    Back navigation fiox

--- app/src/main/java/com/pavelhabzansky/citizenapp/MainActivity.kt
+++ app/src/main/java/com/pavelhabzansky/citizenapp/MainActivity.kt
@@ -1,41 +1,42 @@
 package com.pavelhabzansky.citizenapp
 
-import android.app.Activity
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.pavelhabzansky.citizenapp.core.activity.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import timber.log.Timber
 
 class MainActivity : BaseActivity() {
 
     private lateinit var appBarConfig: AppBarConfiguration
 
+    private val navController: NavController by lazy { findNavController(R.id.navHostFragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Timber.i("Startup")
-
-        val navController = findNavController(R.id.navHostFragment)
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            
-        }
         appBarConfig = AppBarConfiguration(navController.graph, drawer)
 
         setupActionBarWithNavController(navController, appBarConfig)
         navView.setupWithNavController(navController)
-
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.navHostFragment)
         return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            if (!navController.navigateUp()) {
+                super.onBackPressed()
+            }
+        }
+    }
 }
